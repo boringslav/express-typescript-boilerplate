f798501650859188c2464ba35e3803eb0ce1e8f7
Borislav Stoyanov <bostoycontact@gmail.com>
2021-09-06 16:42:16 +0300

    setup

---
+++ src/index.ts
@@ -0,0 +1,11 @@
+import express, { Application } from "express";
+
+const app:Application = express();
+const PORT = process.env.PORT || 8000;
+
+app.get('/', (req, res):void => {
+    res.send("Hello Typescript with Node.js!")
+})
+app.listen(PORT, ():void => {
+    console.log('App listening on port ', PORT);
+})
