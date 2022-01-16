5e1056736d52e32e24108127c3b2d771c243dc8b
burger <bostoycontact@gmail.com>
2022-01-16 11:56:45 +0200

    Setup eslint and prettier

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,11 @@
-import express, { Application } from "express";
+import express, { Application } from 'express';
 
-const app:Application = express();
+const app: Application = express();
 const PORT = process.env.PORT || 8000;
 
-app.get('/', (req, res):void => {
-    res.send("Hello Typescript with Node.js!")
-})
-app.listen(PORT, ():void => {
-    console.log('App listening on port ', PORT);
-})
+app.get('/', (req, res): void => {
+	res.send('Hello Typescript with Node.js!');
+});
+app.listen(PORT, (): void => {
+	console.log('App listening on port ', PORT);
+});
